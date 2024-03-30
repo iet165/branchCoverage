0d4a14195b9edd270e822610ad6e2ac403ee6ef5
ilbeyefe <119613056+iet165@users.noreply.github.com>
2024-03-31 02:09:58 +0300

    Update TestCompute.java

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -1,17 +1,30 @@
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-
 import org.junit.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class TestCompute {
   Compute c;
-
   @Test
   public void example() {
     MessageQueue mq = mock(MessageQueue.class);
     c = new Compute(mq);
     assertTrue(true);
+  }
+  
+  @Test
+  public void testBosQueue() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(0);
+    Compute compute = new Compute(mq);
+    assertEquals(-1, compute.countNumberOfOccurrences("deneme"));
+  }
+
+  @Test
+  public void testQueueElementYok() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.contains("deneme")).thenReturn(false);
+    when(mq.size()).thenReturn(10); 
+    Compute compute = new Compute(mq);
+    assertEquals(0, compute.countNumberOfOccurrences("deneme"));
   }
 }

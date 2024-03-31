dcc5adb1cf7f417b8f3a9447a81b6646c5193ea7
ilbeyefe <119613056+iet165@users.noreply.github.com>
2024-03-31 14:04:08 +0300

    Update TestCompute.java

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -27,4 +27,33 @@ public class TestCompute {
     Compute compute = new Compute(mq);
     assertEquals(0, compute.countNumberOfOccurrences("deneme"));
   }
+
+  @Test
+  public void testQueueElementBirdenFazla() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.contains("element")).thenReturn(true);
+    when(mq.size()).thenReturn(3);
+    when(mq.getAt(0)).thenReturn("element");
+    when(mq.getAt(1)).thenReturn("element");
+    when(mq.getAt(2)).thenReturn("other");
+    Compute compute = new Compute(mq);
+
+    assertEquals(2, compute.countNumberOfOccurrences("element"));
+  }
+
+  @Test
+  public void testQueueElementBirKere() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.contains("element")).thenReturn(true);
+    when(mq.size()).thenReturn(3);
+    when(mq.getAt(0)).thenReturn("other");
+    when(mq.getAt(1)).thenReturn("element");
+    when(mq.getAt(2)).thenReturn("other");
+    Compute compute = new Compute(mq);
+
+    assertEquals(1, compute.countNumberOfOccurrences("element"));
+  }
+
+
+
 }
